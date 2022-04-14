8f41e85571bc8b8ebdf6af4243877d0e8df2e0d9
THREEhundo <samuelbaik87@gmail.com>
2022-04-14 00:04:32 -0700

    shopping page mobile menu added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 const hamburger = document.querySelector('.menu')
-const dropDownMenu = document.querySelector('.drop-down-menu')
-hamburger.addEventListener('click', () => {
-	// if class opened is true
-	// display block -> drop-down-menu
+
+const shoppingMobileMenu = document.querySelector('#shopping-mobile-menu')
+
+const mobileListItems = document.querySelectorAll('.mobile-list-item')
+
+function toggleMobileMenu() {
+	const dropDownMenu = document.querySelector('.drop-down-menu')
 	dropDownMenu.classList.toggle('block')
 	dropDownMenu.classList.toggle('none')
 	document.body.classList.toggle('top-fixed')
-})
+}
+
+hamburger.addEventListener('click', () => toggleMobileMenu())
+
+shoppingMobileMenu.addEventListener('click', () => toggleMobileMenu())
+
+mobileListItems.forEach((item) =>
+	item.addEventListener('click', () => hamburger.classList.toggle('opened')),
+)
